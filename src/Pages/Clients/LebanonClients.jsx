49839080e3f9f49be62a833225f61bf1c15819b5
SaeedTello <ghythtello3@gmail.com>
2026-02-15 14:55:29 +0300

    simple edite

--- src/Pages/Clients/LebanonClients.jsx
+++ src/Pages/Clients/LebanonClients.jsx
@@ -3,6 +3,30 @@ import image1 from "../../assets/img/Lebanon_Clients/2.webp";
 import image2 from "../../assets/img/Lebanon_Clients/3.webp";
 import image3 from "../../assets/img/Lebanon_Clients/4.webp";
 import image4 from "../../assets/img/Lebanon_Clients/5.webp";
+
+const clientsData = [
+  {
+    id: 1,
+    image: image1,
+    name: "Shawrma Akkar",
+  },
+  {
+    id: 2,
+    image: image2,
+    name: "Lebanon Coffee House",
+  },
+  {
+    id: 3,
+    image: image3,
+    name: "Camella",
+  },
+  {
+    id: 4,
+    image: image4,
+    name: "Sandwich & Half",
+  },
+];
+
 export default function LebanonClients() {
   return (
     <>
@@ -12,23 +36,15 @@ export default function LebanonClients() {
           <span>الرئيسية / العملاء / عملاء لبنان</span>
         </div>
         <div className="clients-body my-4 text-center fw-bold">
-          <ul className="clients-cards container w-75">
-            <li className="card my-5">
-              <img src={image1} alt="" />
-              <p className=" my-1">Shawrma Akkar</p>
-            </li>
-            <li className="card my-5">
-              <img src={image2} alt="" />
-              <p className="my-1">Lebanon Coffee House</p>
-            </li>
-            <li className="card my-5">
-              <img src={image3} alt="" />
-              <p className="my-1">Camella</p>
-            </li>
-            <li className="card my-5">
-              <img src={image4} alt="" />
-              <p className="my-1">Sandwich & Half</p>
-            </li>
+          <ul className="clients-cards cards text-center container w-75 my-5">
+            {clientsData.map((client) => (
+              <li className="card p-0" key={client.id}>
+                <img className="card-imag" src={client.image} alt={client.name} />
+                <div className="card-body">
+                  <h5>{client.name}</h5>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
